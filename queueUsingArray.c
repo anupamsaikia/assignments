78d9b0a790d5d524a8a83aca365c2c0c3ce82cc2
Anupam Saikia <ianupamsaikia@gmail.com>
2018-11-23 00:24:57 +0530

    Queue implementation

--- queueUsingArray.c
+++ queueUsingArray.c
@@ -1,76 +1,91 @@
+// C pogram to implement queue using array
+
 #include <stdio.h>
 #include <stdlib.h>
 
 const int max = 10;
-int queue[10];
+int queue[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int front = -1;
 int rear = -1;
 
 void insert(int value)
 {
-  if ((front == 0 && rear == max-1) || (front == rear + 1))
-  {
+  if ((front == 0 && rear == max - 1) || (front == rear + 1))
     printf("Overflow");
-  }
-
   else
   {
     if (front == -1)
-    {
       front = rear = 0;
-    }
-    else if (rear = max-1)
-    {
+    else if (rear == max - 1)
       rear = 0;
-    }
     else
-    {
       rear++;
-    }
-
     queue[rear] = value;
   }
 }
 
-void delete()
+void delete ()
 {
   if (front == -1)
-  {
     printf("Empty Queue");
-  }
   else if (front == rear)
-  {
-    front = -1;
-  }
+    front = rear = -1;
+  else if (front == max-1)
+    front = 0;
   else
-  {
     front++;
-  }
 }
 
 void display()
 {
   int i;
-  if(rear < front){
+  if (rear < front)
+  {
     for (i = front; i < max; i++)
       printf("%d ", queue[i]);
-    for (i = 0; i<rear; i++)
+    for (i = 0; i <= rear; i++)
       printf("%d ", queue[i]);
   }
-  else{
-    for (i = front; i<rear; i++)
+  else
+  {
+    for (i = front; i <= rear; i++)
       printf("%d ", queue[i]);
   }
-  
 }
 
 int main()
 {
-  insert(23);
-  insert(65);
-  insert(5);
-  insert(655);
 
-  printf("%d%d", front, rear);
-  //display();
+  int choice, info, key;
+
+  do
+  {
+    printf("\n\n\nEnter Choice : \n");
+    printf("1.Insert\t2.Delete\t3.Exit\n");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+      printf("\nEnter value :");
+      scanf("%d", &info);
+      system("@cls||clear");
+      insert(info);
+      display();
+      break;
+    case 2:
+      system("@cls||clear");
+      delete ();
+      display();
+      break;
+    case 3:
+      system("@cls||clear");
+      return 0;
+    default:
+      system("@cls||clear");
+      printf("\nWrong Choice\n");
+    }
+  } while (1);
+
+  return 0;
 }
